16a34de190676d43f7c4cdb1136815fb0e2d955d
Erik Wong <k4orta@gmail.com>
2013-01-19 18:52:57 -0800

    Fixed an issue where garbage collection was not cleaning up after the config reader
    -Config reader will now store the contents of a provider's config file for futre requests
    -Added additional error checking in the Twitch and IGN executor files for safety

--- ads/twitch-executer.go
+++ ads/twitch-executer.go
@@ -10,6 +10,12 @@ func (this *TwitchRequest) RunAd() {
 	if debugMode {
 		this.Channel = "ipldev"
 	}
-	http.Post(this.Url+this.Channel+"/commercial?oauth_token="+this.OAuth, "application/json", strings.NewReader(``))
+	resp, err := http.Post(this.Url+this.Channel+"/commercial?oauth_token="+this.OAuth, "application/json", strings.NewReader(``))
+	if err != nil {
+		log.Println("Error posting to Twitch, status: " + err.Error())
+		return
+	}
+
+	resp.Body.Close()
 	log.Println("Fired a Twitch Ad")
 }
